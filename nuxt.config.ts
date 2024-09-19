eb6e2cb3c975582ce7b4f9319432bbb757ef7cdc
FizKw <hafizhkumara.w@gmail.com>
2024-09-20 00:58:29 +0700

    fix: nuxt-config & Dockerfile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     // Make environment variables accessible here
     public: {
-      STUDENT_NAME: process.env.STUDENT_NAME || 'Hafizh Kumara Widyadhana',
-      STUDENT_NIM: process.env.STUDENT_NIM || '00000073191',
+      STUDENT_NAME: process.env.NUXT_STUDENT_NAME,
+      STUDENT_NIM: process.env.NUXT_STUDENT_NIM,
       // Add more environment variables as needed
     },
   },
